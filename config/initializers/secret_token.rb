a35510b194d96897875fb46405e41f3454c488ed
pablo <pienkowskip@gmail.com>
2013-11-30 15:33:09 +0100

    Initial import: plain rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cryptobroker::Application.config.secret_key_base = '4b6a4c773b9b2245af2dbc1b5cf688cb405054b336713462c07125bfd94d2e2bde293dd9786ae592e6591c16f7b279f2636aa522d4bef45ee8f6a97df5f76417'
